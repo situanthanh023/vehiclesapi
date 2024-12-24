58cd5565668d87c301ddc19b0a6ee4d9b2aff3bf
situanthanh023 <55404926+situanthanh023@users.noreply.github.com>
2024-12-24 21:54:35 +0700

    create new project

---
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -0,0 +1,117 @@
+package com.udacity.vehicles.service;
+
+import com.udacity.vehicles.client.maps.MapsClient;
+import com.udacity.vehicles.client.prices.PriceClient;
+import com.udacity.vehicles.domain.car.Car;
+import com.udacity.vehicles.domain.car.CarRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Implements the car service create, read, update or delete
+ * information about vehicles, as well as gather related
+ * location and price data when desired.
+ */
+@Service
+public class CarService {
+
+    private final PriceClient priceClient;
+    private final MapsClient mapsClient;
+    private final CarRepository repository;
+
+    public CarService(CarRepository repository, PriceClient priceClient, MapsClient mapsClient) {
+        this.priceClient = priceClient;
+        this.mapsClient = mapsClient;
+        this.repository = repository;
+    }
+
+    /**
+     * Gathers a list of all vehicles
+     *
+     * @return a list of all vehicles in the CarRepository
+     */
+    public List<Car> list() {
+        return repository.findAll();
+    }
+
+    /**
+     * Gets car information by ID (or throws exception if non-existent)
+     *
+     * @param id the ID number of the car to gather information on
+     * @return the requested car's information, including location and price
+     */
+    public Car findById(Long id) {
+        Optional<Car> optionalCar = repository.findById(id);
+
+        if (optionalCar.isEmpty()) {
+            throw new CarNotFoundException("No car is available with this id");
+        }
+        Car car = new Car();
+
+        try {
+            car = optionalCar.get();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        /**
+         * Note: The car class file uses @transient, meaning you will need to call
+         *   the pricing service each time to get the price.
+         */
+        try {
+            car.setPrice(priceClient.getPrice(id));
+        } catch (Exception e) {
+            System.out.println("Error in setting price" + e.getMessage());
+        }
+
+        /**
+         * Note: The Location class file also uses @transient for the address,
+         * meaning the Maps service needs to be called each time for the address.
+         */
+        try {
+            car.setLocation(mapsClient.getAddress(car.getLocation()));
+        } catch (Exception e) {
+            System.out.println("Error in setting location" + e.getMessage());
+        }
+
+        return car;
+    }
+
+    /**
+     * Either creates or updates a vehicle, based on prior existence of car
+     *
+     * @param car A car object, which can be either new or existing
+     * @return the new/updated car is stored in the repository
+     */
+    public Car save(Car car) {
+        if (car.getId() != null) {
+            return repository.findById(car.getId()).map(carToBeUpdated -> {
+                carToBeUpdated.setDetails(car.getDetails());
+                carToBeUpdated.setLocation(car.getLocation());
+                return repository.save(carToBeUpdated);
+            }).orElseThrow(CarNotFoundException::new);
+        }
+
+        return repository.save(car);
+    }
+
+    /**
+     * Deletes a given car by ID
+     *
+     * @param id the ID number of the car to delete
+     */
+    public void delete(Long id) {
+        Optional<Car> optionalCar = repository.findById(id);
+        if (optionalCar.isEmpty()) {
+            throw new CarNotFoundException("No car is available with this id");
+        }
+
+        Car car = optionalCar.get();
+        repository.deleteById(car.getId());
+
+
+    }
+}
